1d561b350cb4d68f67a6b09a8719028335f5c3ea
Fernando Perry <fernandoperry1234@gmail.com>
2025-08-08 22:19:02 +0700

    fix the internal lua stuff.
    - see example for the change.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -11,6 +12,14 @@
 #include "str.h"
 
 #define MIN_ARGS 3
+
+bool only_whitespace(char *str) {
+    char *ptr = str;
+    while (ptr && *ptr != '\0') {
+        if (!isspace(*ptr++)) return false;
+    }
+    return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -39,45 +48,99 @@ int main(int argc, char **argv)
     long in_size = ftell(in);
     rewind(in);
 
-    str_t out_builder = str_init(in_size * 2);
+    str_t builder = str_init(in_size * 2);
     str_t buffer = str_init(100);
+    str_t final =  str_init(in_size * 2);
 
+    /* start parsing */
+    str_push(&builder, "local out = {}\n");
+
+    bool code = false;
+    bool inline_code = false;
     int c;
-    bool skip = false;
     /* do the work */
     while ((c = fgetc(in)) != EOF) {
         if (c == opt->sep) {
-            skip = !skip;
+            if (inline_code) str_push(&builder, ")");
+            if (!inline_code) str_push_chr(&builder, '\n');
+            inline_code = false;
+            code = !code;
             if (buffer.len > 0) {
-                if (luaL_dostring(L, buffer.data) != LUA_OK) {
-                    fprintf(stderr, "ERR: Lua error: %s\n", lua_tostring(L, -1));
-                    lua_pop(L, 1);
-                } else {
-                    if (lua_gettop(L) >= 1) {
-                        if (lua_isstring(L, -1)) {
-                            const char *result = lua_tostring(L, -1);
-                            str_push(&out_builder, result);
-                        }
-                    }
-                    lua_settop(L, 0);
+                if (only_whitespace(buffer.data)) {
+                    str_clear(&buffer);
+                    continue;
                 }
+                str_push(&builder, "out[#out+1] = \"");
+                str_push(&builder, buffer.data);
+                str_push(&builder, "\"\n");
                 str_clear(&buffer);
             }
             continue;
         }
-        if (!skip && c == '\\') {
-            str_push_chr(&out_builder, fgetc(in));
+        if (code) {
+            if (c == '\\') {
+                int next = fgetc(in);
+                if (!isspace(next))
+                    str_push_chr(&builder, next);
+                continue;
+            }
+            if (c == '$') {
+                inline_code = true;
+                c = fgetc(in);
+                str_push(&builder, "out[#out+1] = ");
+                str_push(&builder, "tostring(");
+            }
+            str_push_chr(&builder, c);
             continue;
+        } else {
+            if (c == '\\') {
+                int next = fgetc(in);
+                if (!isspace(next))
+                    str_push_chr(&builder, next);
+                continue;
+            }
+            if (isspace(c) || c == '"') {
+                switch (c) {
+                    case '\n':
+                        str_push_chr(&buffer, '\\');
+                        str_push_chr(&buffer, 'n');
+                        break;
+                    case '\r':
+                        str_push_chr(&buffer, '\\');
+                        str_push_chr(&buffer, 'r');
+                        break;
+                    case '\t':
+                        str_push_chr(&buffer, '\\');
+                        str_push_chr(&buffer, 't');
+                        break;
+                    case '"':
+                        str_push_chr(&buffer, '\\');
+                        str_push_chr(&buffer, '"');
+                        break;
+                    default:
+                        str_push_chr(&buffer, c);
+                        break;
+                }
+                continue;
+            }
+            str_push_chr(&buffer, c);
         }
-        else if (skip && c == '\\') {
-            str_push_chr(&buffer, fgetc(in));
-            continue;
+    }
+
+    str_push(&builder, "return table.concat(out)\n");
+    /* end parsing */
+
+    if (luaL_dostring(L, builder.data) != LUA_OK) {
+        fprintf(stderr, "ERR: Lua error: %s\n", lua_tostring(L, -1));
+        lua_pop(L, 1);
+    } else {
+        if (lua_gettop(L) >= 1) {
+            if (lua_isstring(L, -1)) {
+                const char *result = lua_tostring(L, -1);
+                str_push(&final, result);
+            }
         }
-        else if (!skip) str_push_chr(&out_builder, c);
-        else {
-            if (c == '$') str_push(&buffer, "return ");
-            else str_push_chr(&buffer, c);
-        }
+        lua_settop(L, 0);
     }
 
     /* write to a file */
@@ -90,13 +153,14 @@ int main(int argc, char **argv)
         lua_close(L);
         return EXIT_FAILURE;
     }
-    fprintf(out, "%s", out_builder.data);
+    fprintf(out, "%s", final.data);
 
     /* closing */
     lua_close(L);
     cleanup_args(opt);
-    str_deinit(&out_builder);
+    str_deinit(&builder);
     str_deinit(&buffer);
+    str_deinit(&final);
     fclose(in);
     fclose(out);
     return EXIT_SUCCESS;
